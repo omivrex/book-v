14663e69d9facc86d0f531d76024a3c0a6a21af0
omivrex <omivrex@gmail.com>
2024-03-12 04:43:15 +0100

    designed booking screen

--- components/screens/main/Bookings.screen.tsx
+++ components/screens/main/Bookings.screen.tsx
@@ -1,12 +1,105 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import Container from "../../reusables/Containers.component";
 import TextComponent from "../../reusables/Text.component";
+import { Agenda } from "react-native-calendars";
+import colors from "../../../constants/colors.context";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
+import moment from "moment";
+import { useState } from "react";
 
 const BookingScreen = () => {
+    const [selectedDay, setselectedDay] = useState(moment(new Date().getTime()).format("YYYY-MM-DD"));
+    const appointments = {
+        "2024-03-11": [
+            {
+                name: "Hair Dressing Appointment",
+                height: 50,
+                day: "2024-03-11",
+                description: "Discuss project requirements and timelines.",
+            },
+            {
+                name: "Meeting with Client",
+                height: 50,
+                day: "2024-03-11",
+                description: "Discuss project requirements and timelines.",
+            },
+        ],
+
+        "2024-03-12": [
+            {
+                name: "Team Standup",
+                height: 50,
+                day: "2024-03-12",
+                description: "Catch up with the team on project progress.",
+            },
+
+            {
+                name: "Team Standup",
+                height: 50,
+                day: "2024-03-12",
+                description: "Catch up with the team on project progress.",
+            },
+        ],
+
+        "2024-03-13": [
+            {
+                name: "Team Standup",
+                height: 50,
+                day: "2024-03-13",
+                description: "Catch up with the team on project progress.",
+            },
+        ],
+    };
+
+    const renderItem = ({ item }: any) => {
+        return (
+            <View style={{ backgroundColor: colors.yellow, padding: "5%", marginVertical: "5%" }}>
+                <TextComponent color={colors.white}>{item.name}</TextComponent>
+                <TextComponent color={colors.white} fontFamily="Poppins_300Light">
+                    {item.description}
+                </TextComponent>
+            </View>
+        );
+    };
+
     return (
         <Container>
             <View>
-                <TextComponent>Bookings Screen</TextComponent>
+                <Agenda
+                    theme={{
+                        agendaDayTextColor: colors.red,
+                        agendaDayNumColor: colors.red,
+                        agendaTodayColor: colors.blue,
+                        agendaKnobColor: colors.white,
+                        selectedDayBackgroundColor: colors.grey7,
+                        selectedDayTextColor: colors.yellow,
+                        textSectionTitleColor: colors.white,
+                        calendarBackground: colors.black,
+                        textDayFontFamily: "Poppins_300Light",
+                        textDayFontSize: hp("2%"),
+                        textDayFontWeight: "300",
+                    }}
+                    onDayPress={({ timestamp }) => {
+                        setselectedDay(moment(timestamp).format("YYYY-MM-DD"));
+                    }}
+                    renderList={({ items }) => {
+                        return (
+                            <FlatList
+                                data={items ? items[moment(selectedDay).format("YYYY-MM-DD")] : []}
+                                renderItem={renderItem}
+                                keyExtractor={(item, index) => index.toString()}
+                                style={{
+                                    backgroundColor: colors.black,
+                                    height: hp("60%"),
+                                }}
+                            />
+                        );
+                    }}
+                    showOnlySelectedDayItems
+                    items={appointments}
+                    showClosingKnob
+                    style={{ width: wp("100%") }}
+                />
             </View>
         </Container>
     );

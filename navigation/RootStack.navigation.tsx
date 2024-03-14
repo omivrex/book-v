f097db82ca25f6f42337402e4c001b355e88ac05
omivrex <omivrex@gmail.com>
2024-03-14 03:58:56 +0100

    code clean up

--- navigation/RootStack.navigation.tsx
+++ navigation/RootStack.navigation.tsx
@@ -11,8 +11,6 @@ const Stack = createNativeStackNavigator<RootStackType>();
 export default function RootStack() {
     const userDataContext = useContext(UserDataContext);
 
-    console.log("test=>", userDataContext?.userData);
-
     return (
         <Stack.Navigator initialRouteName={userDataContext?.userData ? "Main" : "Auth"} screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Auth" component={AuthStack} />

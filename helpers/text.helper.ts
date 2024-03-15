455997e832b5763fcd2f57529f67b2fe0612b83d
omivrex <omivrex@gmail.com>
2024-03-15 05:19:10 +0100

    uodated text helper function

--- helpers/text.helper.ts
+++ helpers/text.helper.ts
@@ -1,12 +1,20 @@
-export const capitalize1stLetterOfEachWord = (sentence: string): string => {
-    const words = sentence?.split(" ");
-    words?.map((word, index) => {
-        words[index] = [word[0]?.toLocaleUpperCase(), ...word?.slice(1, word?.length).split("")].join("");
-    });
-    return words?.join(" ");
+export const capitalize1stLetterOfEachWord = (sentence?: string): string => {
+    if (sentence && typeof sentence === "string") {
+        const words = sentence?.split(" ");
+        words?.map((word, index) => {
+            words[index] = [word[0]?.toLocaleUpperCase(), ...word?.slice(1, word?.length).split("")].join("");
+        });
+        return words?.join(" ");
+    } else {
+        return "";
+    }
 };
 
-export const truncateString = (string: string, returnLength: number = 10): string => {
-    const formattedString = string?.slice(0, returnLength);
-    return string?.length > formattedString?.length ? formattedString + "..." : formattedString;
+export const truncateString = (string?: string, returnLength: number = 10): string => {
+    if (string && typeof string === "string") {
+        const formattedString = string?.slice(0, returnLength);
+        return string?.length > formattedString?.length ? formattedString + "..." : formattedString;
+    } else {
+        return "";
+    }
 };
